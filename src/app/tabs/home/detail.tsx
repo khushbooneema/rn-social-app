5c9c06158216c6bbaac70042d4d001929d8f0875
Khushboo neema <khushboo.neema@sherwin.com>
2026-08-24 15:30:27 -0400

    text change

--- src/app/tabs/home/detail.tsx
+++ src/app/tabs/home/detail.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function DetailScreen() {
     return (
         <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text>Detail Screen</Text>
+            <Text>Wrok in progress</Text>
         </View>
     )
 }
